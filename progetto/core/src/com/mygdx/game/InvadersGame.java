1b73835a273e16f60cbca6f489f4df9af06d4727
ChriTorr <christian.torli@itiszuccante.edu.it>
2023-12-06 13:40:00 +0100

    .

--- progetto/core/src/com/mygdx/game/InvadersGame.java
+++ progetto/core/src/com/mygdx/game/InvadersGame.java
@@ -1,8 +1,6 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
