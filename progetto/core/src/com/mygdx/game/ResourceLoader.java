118472717578664ba18b8f595eb168317e2e422a
ChriTorr <christian.torli@itiszuccante.edu.it>
2024-01-15 15:44:48 +0100

    .

--- progetto/core/src/com/mygdx/game/ResourceLoader.java
+++ progetto/core/src/com/mygdx/game/ResourceLoader.java
@@ -18,79 +18,79 @@ public class ResourceLoader {
         if(!mapTexture.containsKey(index)){
             switch (index) {
             case STEALTH_PLANE:
-                mapTexture.put(index, new Texture("progetto/assets/stealth_plane.png"));
+                mapTexture.put(index, new Texture("../assets/stealth_plane.png"));
             break;
             case WAR_PLANE:
-                mapTexture.put(index, new Texture("progetto/assets/war_plane.png"));
+                mapTexture.put(index, new Texture("../assets/war_plane.png"));
             break;
             case SKY:
-                mapTexture.put(index, new Texture("progetto/assets/sky.png"));
+                mapTexture.put(index, new Texture("../assets/sky.png"));
             break;
             case BUBBLE:
-                mapTexture.put(index, new Texture("progetto/assets/bubble.png"));
+                mapTexture.put(index, new Texture("../assets/bubble.png"));
             break;
             case MENU:
-                mapTexture.put(index, new Texture("progetto/assets/menu.png"));
+                mapTexture.put(index, new Texture("../assets/menu.png"));
             break;
             case STEALTH_BULLET:
-                mapTexture.put(index, new Texture("progetto/assets/stealth_bullet.png"));
+                mapTexture.put(index, new Texture("../assets/stealth_bullet.png"));
             break;
             case WAR_BULLET:
-                mapTexture.put(index, new Texture("progetto/assets/war_bullet.png"));
+                mapTexture.put(index, new Texture("../assets/war_bullet.png"));
             break;
             case HEALTH_1:
-                mapTexture.put(index, new Texture("progetto/assets/Health1.png"));
+                mapTexture.put(index, new Texture("../assets/Health1.png"));
             break;
             case HEALTH_2:
-                mapTexture.put(index, new Texture("progetto/assets/Health2.png"));
+                mapTexture.put(index, new Texture("../assets/Health2.png"));
             break;
             case HEALTH_3:
-                mapTexture.put(index, new Texture("progetto/assets/Health3.png"));
+                mapTexture.put(index, new Texture("../assets/Health3.png"));
             break;
             case FALLING_HEART:
-                mapTexture.put(index, new Texture("progetto/assets/falling_heart.png"));
+                mapTexture.put(index, new Texture("../assets/falling_heart.png"));
             break;
             case CHEST:
-                mapTexture.put(index, new Texture("progetto/assets/chest.png"));
+                mapTexture.put(index, new Texture("../assets/chest.png"));
             break;
             case SCORE_FRAME:
-                mapTexture.put(index, new Texture("progetto/assets/score_frame.png"));
+                mapTexture.put(index, new Texture("../assets/score_frame.png"));
             break;
             case ZERO:
-                mapTexture.put(index, new Texture("progetto/assets/zero.png"));
+                mapTexture.put(index, new Texture("../assets/zero.png"));
             break;
             case ONE:
-                mapTexture.put(index, new Texture("progetto/assets/one.png"));
+                mapTexture.put(index, new Texture("../assets/one.png"));
             break;
             case TWO:
-                mapTexture.put(index, new Texture("progetto/assets/two.png"));
+                mapTexture.put(index, new Texture("../assets/two.png"));
             break;
             case THREE:
-                mapTexture.put(index, new Texture("progetto/assets/three.png"));
+                mapTexture.put(index, new Texture("../assets/three.png"));
             break;
             case FOUR:
-                mapTexture.put(index, new Texture("progetto/assets/four.png"));
+                mapTexture.put(index, new Texture("../assets/four.png"));
             break;
             case FIVE:
-                mapTexture.put(index, new Texture("progetto/assets/five.png"));
+                mapTexture.put(index, new Texture("../assets/five.png"));
             break;
             case SIX:
-                mapTexture.put(index, new Texture("progetto/assets/six.png"));
+                mapTexture.put(index, new Texture("../assets/six.png"));
             break;
             case SEVEN:
-                mapTexture.put(index, new Texture("progetto/assets/seven.png"));
+                mapTexture.put(index, new Texture("../assets/seven.png"));
             break;
             case EIGHT:
-                mapTexture.put(index, new Texture("progetto/assets/eight.png"));
+                mapTexture.put(index, new Texture("../assets/eight.png"));
             break;
             case NINE:
-                mapTexture.put(index, new Texture("progetto/assets/nine.png"));
+                mapTexture.put(index, new Texture("../assets/nine.png"));
             break;
             case YOU_LOSE:
-                mapTexture.put(index, new Texture("progetto/assets/you_lose.png"));
+                mapTexture.put(index, new Texture("../assets/you_lose.png"));
             break;
             case YOU_WIN:
-                mapTexture.put(index, new Texture("progetto/assets/you_win.png"));
+                mapTexture.put(index, new Texture("../assets/you_win.png"));
             break;
 
             default: return null;
@@ -137,27 +137,27 @@ public class ResourceLoader {
         if(!mapSound.containsKey(index)){
             switch (index) {
             case MUSIC_LEVEL:
-                res = Gdx.audio.newSound(Gdx.files.internal("progetto/sounds/music_level.mp3"));
+                res = Gdx.audio.newSound(Gdx.files.internal("../sounds/music_level.mp3"));
                 mapSound.put(index, res);
             break;
             case EXPLOSION_SOUND:
-                res = Gdx.audio.newSound(Gdx.files.internal("progetto/sounds/explosion.mp3"));
+                res = Gdx.audio.newSound(Gdx.files.internal("../sounds/explosion.mp3"));
                 mapSound.put(index, res);
             break;
             case FIREBALL_SOUND:
-                res = Gdx.audio.newSound(Gdx.files.internal("progetto/sounds/fireball.mp3"));
+                res = Gdx.audio.newSound(Gdx.files.internal("../sounds/fireball.mp3"));
                 mapSound.put(index, res);
             break;
             case PROJECTILE_SOUND:
-                res = Gdx.audio.newSound(Gdx.files.internal("progetto/sounds/projectile_sound.mp3"));
+                res = Gdx.audio.newSound(Gdx.files.internal("../sounds/projectile_sound.mp3"));
                 mapSound.put(index, res);
             break;
             case CHEST_SOUND:
-                res = Gdx.audio.newSound(Gdx.files.internal("progetto/sounds/chest.mp3"));
+                res = Gdx.audio.newSound(Gdx.files.internal("../sounds/chest.mp3"));
                 mapSound.put(index, res);
             break;
             case MENU_MUSIC:
-                res = Gdx.audio.newSound(Gdx.files.internal("progetto/sounds/menu_music.mp3"));
+                res = Gdx.audio.newSound(Gdx.files.internal("../sounds/menu_music.mp3"));
                 mapSound.put(index, res);
             break;
             default: res = null;
